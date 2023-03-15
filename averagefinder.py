927203c5791bc1ad8603643f3cabf7bdcbe37861
rey <114239691+indigowitchh@users.noreply.github.com>
2023-03-15 08:21:39 -0600

    Create averagefinder.py

---
+++ averagefinder.py
@@ -0,0 +1,8 @@
+#Medium----------------------------------------------
+
+nums = [20, 5, 13, 15]
+
+def averagefinder(num1, num2, num3, num4):
+    print(num1+num2+num3+num4/4)
+    
+averagefinder(nums[0],nums[1],nums[2],nums[3])
